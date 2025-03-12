a97c4c5032a546d98520c18c420fce5bbdfd880c
Adelia-Kirana <adeliak.0208@gmail.com>
2025-03-12 21:37:54 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {},
+    colors:{
+      primary:"#4f46e5",
+      dark : "#0f172a",
+      secondary : "#64748b",
+      slate : "#f1f5f9",
+    },
+  },
+  plugins: [],
+}
